a6c69d55a51cd4da61f5aab3d587acafdf17d186
solos-pro <sergey-solonichenko@rambler.ru>
2022-02-05 15:07:07 +0300

    pretty table is used

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import raw_data
 import json
 import prettytable
 
-
 app = Flask(__name__, template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -41,8 +40,8 @@ class Offer(db.Model):
     __tablename__ = 'offer'
 
     id = db.Column(db.Integer, primary_key=True)
-    order_id = db.Column(db.Integer, db.ForeignKey('order.id'))
-    executor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
+    executor_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def to_dict(self):
         return {
@@ -81,7 +80,6 @@ class Order(db.Model):
 
 db.create_all()
 
-
 for data_user in raw_data.users:
     new_user = User(
         id=data_user["id"],
@@ -119,16 +117,33 @@ for data_offer in raw_data.offers:
     db.session.commit()
 
 
-@app.route("/users", methods=['GET', ])
+@app.route("/users", methods=['GET', 'POST'])
 def users():
     if request.method == "GET":
         res = []
         for u in User.query.all():
             res.append(u.to_dict())
         return json.dumps(res), 200, CONTENT_TYPE
+    elif request.method == "POST":  # adding
+        u = User(first_name=request.form.get('first_name'),
+                 last_name=request.form.get('last_name'),
+                 age=request.form.get('age'),
+                 email=request.form.get('email'),
+                 role=request.form.get('role'),
+                 phone=request.form.get('phone'))
+        db.session.add(u)
+        db.session.commit()
+
+        mytable = prettytable.PrettyTable()
+        mytable.field_names = ['id', 'first_name', 'last_name', 'age', 'email', 'role', 'phone']
+        mytable.max_width = 25
+        rows = [[x.id, x.first_name, x.last_name, x.age, x.email, x.role, x.phone] for x in do_request_user_add()]
+        mytable.add_rows(rows)
+        print(mytable)
+        return "", 204
 
 
-@app.route("/users/<int:uid>", methods=['GET', 'POST', 'DELETE', 'PUT'])
+@app.route("/users/<int:uid>", methods=['GET', 'DELETE', 'PUT'])
 def user(uid):
     if request.method == "GET":
         return json.dumps(User.query.get(uid).to_dict()), 200, CONTENT_TYPE
@@ -137,7 +152,7 @@ def user(uid):
         db.session.delete(del_user)
         db.session.commit()
         return "", 204
-    elif request.method == "PUT":   # updating
+    elif request.method == "PUT":  # updating
         u = User.query.get(uid)
         # user_data = json.loads(request.form)
         u.first_name = request.form.get('first_name')
@@ -148,44 +163,104 @@ def user(uid):
         u.phone = request.form.get('phone')
         db.session.add(u)
         db.session.commit()
-        return "", 204
-    elif request.method == "POST":  # adding
-        u = User(first_name=request.form.get('first_name'),
-                 last_name=request.form.get('last_name'),
-                 age=request.form.get('age'),
-                 email=request.form.get('email'),
-                 role=request.form.get('role'),
-                 phone=request.form.get('phone'))
-        db.session.add(u)
-        db.session.commit()
-        rows = [[x.id, x.first_name, x.last_name,
-                 x.age, x.email, x.role, x.phone] for x in do_request()]
+
+        mytable = prettytable.PrettyTable()
+        mytable.field_names = ['id', 'first_name', 'last_name', 'age', 'email', 'role', 'phone']
+        mytable.max_width = 25
+        rows = [[x.id, x.first_name, x.last_name, x.age, x.email, x.role, x.phone] for x in do_request_user_update()]
         mytable.add_rows(rows)
         print(mytable)
+
+        return "", 204
+
+
+@app.route("/orders", methods=['GET', 'POST'])
+def orders():
+    if request.method == "GET":
+        res = []
+        for o in Order.query.all():
+            res.append(o.to_dict())
+        return json.dumps(res), 200, CONTENT_TYPE
+    elif request.method == "POST":  # adding
+        o = Order(name=request.form.get('name'),
+                  description=request.form.get('description'),
+                  start_date=request.form.get('start_date'),
+                  end_date=request.form.get('end_date'),
+                  address=request.form.get('address'),
+                  customer_id=request.form.get('customer_id'),
+                  executor_id=request.form.get('executor_id'),
+                  )
+        db.session.add(o)
+        db.session.commit()
+
+        mytable = prettytable.PrettyTable()
+        mytable.field_names = ['id', 'name', 'description', 'start_date', 'end_date',
+                               'address', 'customer_id', 'executor_id']
+        mytable.max_width = 30
+        rows = [[x.id, x.name, x.description, x.start_date, x.end_date, x.address, x.customer_id,
+                 x.executor_id] for x in do_request_order_add()]
+        mytable.add_rows(rows)
+        print(mytable)
+
+        return "", 204
+
+
+@app.route("/orders/<int:uid>", methods=['GET', 'DELETE', 'PUT'])
+def order(uid):
+    if request.method == "GET":
+        return json.dumps(User.query.get(uid).to_dict()), 200, CONTENT_TYPE
+    elif request.method == "DELETE":
+        del_order = Order.query.get(uid)
+        db.session.delete(del_order)
+        db.session.commit()
+        return "", 204
+    elif request.method == "PUT":  # updating
+        o = Order.query.get(uid)
+        o.name = request.form.get('name')
+        o.description = request.form.get('description')
+        o.start_date = request.form.get('start_date')
+        o.end_date = request.form.get('end_date')
+        o.address = request.form.get('address')
+        o.price = request.form.get('price')
+        o.customer_id = request.form.get('customer_id')
+        o.executor_id = request.form.get('executor_id')
+        db.session.add(o)
+        db.session.commit()
+
+        mytable = prettytable.PrettyTable()
+        mytable.field_names = ['id', 'name', 'description', 'start_date', 'end_date',
+                               'address', 'customer_id', 'executor_id']
+        mytable.max_width = 30
+        rows = [[x.id, x.name, x.description, x.start_date, x.end_date, x.address, x.customer_id,
+                 x.executor_id] for x in do_request_order_update()]
+        mytable.add_rows(rows)
+        print(mytable)
+
         return "", 204
 
 
 @app.route("/test_test", methods=['GET', 'POST'])
-def testtt():
+def test():
     if request.method == "GET":
         return render_template("test.html")
 
 
-def do_request():
-    result = db.session.query(User).limit(5).all()
+def do_request_user_add():
+    result = db.session.query(User).limit(5).offset(28).all()
+    return result
+
+def do_request_order_add():
+    result = db.session.query(Order).limit(5).offset(47).all()
     return result
 
 
-mytable = prettytable.PrettyTable()
-mytable.field_names = [
-    'id', 'first_name', 'last_name',
-    'age', 'email', 'role', 'phone']
+def do_request_user_update():
+    result = db.session.query(User).limit(5).all()
+    return result
 
-rows = [[x.id, x.first_name, x.last_name,
-         x.age, x.email, x.role, x.phone] for x in do_request()]
-mytable.add_rows(rows)
-mytable.max_width = 25
-
+def do_request_order_update():
+    result = db.session.query(Order).limit(5).all()
+    return result
 
 # print('Запрос возвращает следующие записи:')
 # print(mytable)
